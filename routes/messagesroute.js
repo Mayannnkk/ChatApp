d1eeae96a51fcc7efabaf8ef8eeb97a624a444f3
Mayannnkk <96884389+Mayannnkk@users.noreply.github.com>
2024-01-21 19:20:39 +0530

    Update messagesroute.js

--- routes/messagesroute.js
+++ routes/messagesroute.js
@@ -1,9 +1,11 @@
-const { addmessage,getallmessage, clearallchats} = require("../controllers/messagescontroller")
+const { addmessage,getallmessage, clearallchats, addimage} = require("../controllers/messagescontroller")
+// const  {upload} =require('../index')
 const router = require("express").Router();
 
+// const up=upload
 router.post("/addmsg", addmessage);
 router.post("/getmsg", getallmessage);
 router.post("/clrmsg", clearallchats);
-
+// router.post("/addimage",up.single("image"),(req,res)=>{console.log(req.file)});
 
 module.exports = router;      
